342e78d29e91676dedbe36bb99e849955cb5ea75
felipesantiago95 <felipe.palan@udla.edu.ec>
2024-07-18 12:21:59 -0500

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./src/**/*.{vue,js,ts,jsx,tsx}', './public/index.html'],
+  content: ['./src/**/*.{vue,js,ts,jsx,tsx}', './public/index.html'],
   darkMode: media, // or 'media' or 'class'
   theme: {
     extend: {},
